44074e08c11a20fd1695ca90e00c95d05d7e4ffe
Aidan Jones <ogonivanovich@gmail.com>
2022-06-16 13:51:16 -0500

    Added more queries
    Added to queries:
    - passageQuery
    - searchForBookCodesQuery
    - searchForPassagesQuery

--- src/queries/index.js
+++ src/queries/index.js
@@ -1,5 +1,13 @@
-import catalogQuery from "./catalog.js";
+import catalogQuery from "./catalog";
+import passageQuery from "./passage";
+import searchForBookCodesQuery from "./searchForBookCodes";
+import searchForPassagesQuery from "./searchForPassage";
 
-const queries = {catalogQuery};
+const queries = {
+    catalogQuery, 
+    passageQuery, 
+    searchForBookCodesQuery,
+    searchForPassagesQuery
+};
 
 export default queries;
