345db81c3284f624312e9879742e7f929d0cf1ce
Mark Howe <mvahowe@gmail.com>
2022-06-24 11:30:02 +0200

    Removing module type

--- test/code/queries.cjs
+++ test/code/queries.cjs
@@ -1,6 +1,6 @@
-import tape from 'tape';
-import { queries, postQueries } from '../../src/index.js';
-import { pkWithDocs } from '../lib/load.cjs';
+const tape = require('tape');
+const { queries, postQueries } = require('../../dist/index.js');
+const { pkWithDocs } = require('../lib/load.cjs');
 
 const pk = pkWithDocs([
     ['../test_data/lsv-exo20.usfm', {
@@ -42,9 +42,9 @@ tape(
         const res1 = await pk.gqlQuery(pq1);
         //console.log(JSON.stringify(res1, null, 2));
         t.ok(res1.data.documents[0].cv[0].text.startsWith("In the beginning"), "reference");
-        
-        const cv = postQueries.parseScopeLabels({ 
-            scopeLabels: res1.data.documents[0].cv[0].scopeLabels 
+
+        const cv = postQueries.parseScopeLabels({
+            scopeLabels: res1.data.documents[0].cv[0].scopeLabels
         });
         //console.log(JSON.stringify(cv, null, 2));
         t.ok(cv.chapter === "1" && cv.verse === "1", "John 1:1");
@@ -56,7 +56,7 @@ tape(
         const res2 = await pk.gqlQuery(pq2);
         //console.log(JSON.stringify(res2, null, 2));
         t.ok(res2.data.documents[0].cv[0].scopeLabels[0] === "chapter/21", "bookCode and chapter");
-        
+
         const passage = postQueries.parsePassageResponse({
             bookCode: "REV",
             data: res2.data
@@ -77,21 +77,21 @@ tape(
 tape(
     `test searchForBookCodesQuery`,
     async t => {
-        const sbq1 = queries.searchForBookCodesQuery({ 
+        const sbq1 = queries.searchForBookCodesQuery({
             text: "with him",
-            docSetId: "eng_web" 
+            docSetId: "eng_web"
         });
         const res1 = await pk.gqlQuery(sbq1);
         //console.log(JSON.stringify(res1, null, 2));
         t.ok(res1.data.docSet.documents.length === 2, "plaintext search");
-        
+
         const codes = postQueries.searchForBookCodesFilter({ data: res1.data });
         //console.log(JSON.stringify(codes, null, 2));
         t.deepEqual(codes, ["PSA","JHN"], "Psalms and John");
 
-        const sbq2 = queries.searchForBookCodesQuery({ 
+        const sbq2 = queries.searchForBookCodesQuery({
             text: "beginning strong:G0746", //currently cannot just search for a book code with just a strong
-            docSetId: "eng_web" 
+            docSetId: "eng_web"
         });
         const res2 = await pk.gqlQuery(sbq2);
         //console.log(JSON.stringify(res2, null, 2));
@@ -102,38 +102,38 @@ tape(
 tape(
     `test searchForPassagesQuery`,
     async t => {
-        const spq1 = queries.searchForPassagesQuery({ 
+        const spq1 = queries.searchForPassagesQuery({
             text: "God",
             docSetId: "eng_lsv",
-            bookCode: "REV" 
+            bookCode: "REV"
         });
         const res1 = await pk.gqlQuery(spq1);
         //console.log(JSON.stringify(res1, null, 2));
         t.ok(res1.data.docSet.matches.length === 3, "no blocks or tokens");
-        
+
         const passages = postQueries.searchForVersesFilter({ data: res1.data });
         //console.log(JSON.stringify(passages, null, 2));
         t.ok(passages.length === 8, "8 matching verses found");
 
-        const spq2 = queries.searchForPassagesQuery({ 
+        const spq2 = queries.searchForPassagesQuery({
             text: "Word",
             docSetId: "eng_web",
             bookCode: "JHN",
-            blocks: true 
+            blocks: true
         });
         const res2 = await pk.gqlQuery(spq2);
         //console.log(JSON.stringify(res2, null, 2));
         t.ok(res2.data.docSet.document.mainSequence.blocks.length === 2, "with blocks");
-        
+
         const blocks = postQueries.searchForBlocksFilter({ data: res2.data });
         //console.log(JSON.stringify(blocks, null, 2));
         t.ok(blocks.length === 2, "2 blocks found");
 
-        const spq3 = queries.searchForPassagesQuery({ 
+        const spq3 = queries.searchForPassagesQuery({
             text: "strong:H1984",
             docSetId: "eng_web",
             bookCode: "PSA",
-            tokens: true 
+            tokens: true
         });
         const res3 = await pk.gqlQuery(spq3);
         //console.log(JSON.stringify(res3, null, 2));
